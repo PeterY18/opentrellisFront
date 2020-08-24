cb4e93b09c7f9eb6f3760ca9a3db166fe8f375c7
Peter Yuan <peter.yuan18@gmail.com>
2020-08-24 09:21:45 -0400

    last backup, new design coming soon

--- opentrellisFront/static/js/base.js
+++ opentrellisFront/static/js/base.js
@@ -1,6 +1,34 @@
-$(function () {
-  $(document).scroll(function () {
-    var $nav = $(".navbar-fixed-top");
-    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+let animateElement;
+let prevRatio = 0.0;
+
+window.addEventListener("load", (event) => {
+  animateElement = document.querySelector("#tools");
+}, false);
+
+function createObserver() {
+  let observer;
+
+  let options = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 1.0
+  };
+
+  observer = new IntersectionObserver(handleIntersect, options);
+  observer.observe(animateElement);
+}
+
+function handleIntersect(entries, observer) {
+  entries.forEach((entry) => {
+    if (entry.IntersectionRatio > prevRatio) {
+      entry.target.style.animationName = "fadein";
+      document.getElementById("tools").style.animationName = "fadein";
+    }
+    else {
+      entry.target.style.animationName = "fadein";
+      document.getElementById("tools").style.animationName = "fadein";
+    }
+    prevRatio = entry.IntersectionRatio;
   });
-});
+}
+      document.getElementById("tools").style.animationName = "fadein";
