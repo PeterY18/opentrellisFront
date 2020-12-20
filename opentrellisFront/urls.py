2202d721d4231eb6482380ab5ae8b6bacdca2afb
Peter Yuan <pyuan@pop-os.localdomain>
2020-12-20 16:32:11 -0500

    updated urls to match learn, borrow, invest

--- opentrellisFront/urls.py
+++ opentrellisFront/urls.py
@@ -6,9 +6,9 @@ from . import views
 urlpatterns = [
     path('opentrellis/', views.index, name='index'),
     path('', views.home, name='home'),
-    path('learning/', views.learning, name='learning'),
-    path('lending/', views.lending, name='lending'),
-    path('investing/', views.investing, name='investing'),
+    path('learn/', views.learning, name='learning'),
+    path('borrow/', views.lending, name='lending'),
+    path('invest/', views.investing, name='investing'),
     path('apply/', views.apply, name='apply'),
     path('received/', views.received, name='received'),
     path('legal/', views.legal, name='legal'),
